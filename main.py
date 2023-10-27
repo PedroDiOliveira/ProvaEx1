3a2b8fb6c62ccb45707d977b4137e45bc7b4ba4d
PedroDiOliveira <126473107+PedroDiOliveira@users.noreply.github.com>
2023-10-27 11:28:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,9 @@
 import os
 import time
 import re
+import matplotlib.pyplot as plt
 
-caminhoLogs = 'C:\\Users\\pedro\\exercicio1\\logs.txt'
+caminhoLogs = 'C:\\Users\\pedro\\ProvaEx1\\logs.txt'
 
 #######################################
 ##Funcao que cria a interdace do menu##
@@ -29,6 +30,7 @@ _________________________________________________
 |    2- Mostrar produtos                         |
 |    3- Calculo do faturamento                   |
 |    4- Percentual de vendas                     |
+|    5- Grafico dos 5 maiores valores            |
 |________________________________________________|
     
 Escolha uma opcao para prosseguir:
@@ -39,13 +41,15 @@ Escolha uma opcao para prosseguir:
 ###################################################################
 
 def get_dados_produtos():
-    produtos = {}
+    produtos = []
     j = 0
-    for i in range(3):
-        quantidade = int(input(f"Digite a quantidade de produtos vendidos {i}: "))
-        preco = int(input(f"Digite o valor referente ao produto {i}: "))
-        produtos[quantidade] = preco
+    for i in range(10):  # MUDE O NÚMERO PARA VARIAR A QUANTIDADE DE PRODUTOS A SER ADICIONADOS
+        quantidade = int(input(f"Digite a quantidade de produtos {j} vendidos: "))
+        preco = int(input(f"Digite o valor referente ao produto {j}: "))
+        produtos.append((quantidade, preco))
+        j += 1
     return produtos
+
 
 ##################################################
 ##Funcao que printa na tela o resultado do input##  -> Elemento 2 do menu
@@ -83,19 +87,36 @@ def animacao():
 def salva_Arquivo(produtos, caminhoLogs):
     i = 0
     if not verificaArquivoVazio(caminhoLogs):
-        sobrepor = int(input("Parece que já existe um save anterior! Deseja sobrepor o antigo save? (1-sim / 2-não)"))
-        if sobrepor == 1:
-            animacao()
-            with open(caminhoLogs, 'w') as arquivo:
-                arquivo.write('')
-            with open(caminhoLogs, 'a') as arquivo:  
-                for quantidade, preco in produtos.items():
-                    arquivo.write(f"Produto {i}: Quantidade vendida: {quantidade}   Valor: R${preco:.2f}\n")
-                    i += 1 
-            print("")
-            print("Valores atualizados com sucesso! ")
-        else:
-            print("")
+        while True:
+            try:
+                sobrepor = int(input("Parece que já existe um save anterior! Deseja sobrepor o antigo save? (1-sim / 2-não): "))
+                if sobrepor == 1:
+                    animacao()
+                    with open(caminhoLogs, 'w') as arquivo:
+                        arquivo.write('')
+                    with open(caminhoLogs, 'a') as arquivo:
+                        for i, (quantidade, preco) in enumerate(produtos):
+                            arquivo.write(f"Produto {i}: Quantidade vendida: {quantidade}   Valor: R${preco:.2f}\n")
+                        print("Valores atualizados com sucesso!")
+                    break
+                elif sobrepor == 2:
+                    print("Operação de salvamento cancelada.")
+                    break
+                else:
+                    print("Opção inválida. Por favor, escolha 1 para sim ou 2 para não.")
+            except ValueError:
+                print("Opção inválida. Por favor, escolha 1 para sim ou 2 para não.")
+    else:
+        animacao()
+        with open(caminhoLogs, 'a') as arquivo:
+            for i, (quantidade, preco) in enumerate(produtos):
+                arquivo.write(f"Produto {i}: Quantidade vendida: {quantidade}   Valor: R${preco:.2f}\n")
+        os.system('cls')
+        print("Valores atualizados com sucesso!")
+        time.sleep(1.5)
+
+
+
 
 ###############################################
 ##Funcao que calcula o faturamento da empresa##
@@ -163,6 +184,32 @@ def calcula_Percentual(produtos):#repetindo a operacao para nao precisar usar do
 
     return resultado
 
+
+########################################################################
+##Funcao que cria um interface de um grafico para os 5 maiores valores##
+########################################################################
+
+#E necessario instalar a bibliotecas matPlotLib colando o seguinte codigo no terminal "pip install matplotlib"
+
+
+def gerar_grafico_matplotlib(produtos, n=5):
+    produtos_ordenados = sorted(produtos, key=lambda produto: produto.quantidade, reverse=True)
+    top_produtos = produtos_ordenados[:n]
+
+    quantidade = [produto.quantidade for produto in top_produtos]
+    nomes = [f"Produto {produto.numero}" for produto in top_produtos]
+
+    plt.figure(figsize=(10, 6))
+    plt.barh(nomes, quantidade, color='blue')
+    plt.xlabel('Quantidade Vendida')
+    plt.title('5 Maiores Valores')
+
+    for i, v in enumerate(quantidade):
+        plt.text(v, i, str(v), va='center', color='black', fontweight='bold')
+
+    plt.tight_layout()
+    plt.show()
+
         
 ###############################################
 ##Funcao menu que gerencia as funcoes criadas##
@@ -172,30 +219,42 @@ def menu():
     sair = False
     while not sair:
         interface_menu()
-        opcao = int(input("Digite uma opcao: "))
-        if opcao == 1:
-            produtos = get_dados_produtos()
-            salva_Arquivo(produtos, caminhoLogs)
-            os.system('cls')
-        elif opcao == 2:
-            os.system('cls')
-            y = leituraArquivo(caminhoLogs)
-            x = mostra_produtos(y)
-            print(x)
-        elif opcao == 3:
-            animacao()
-            y = leituraArquivo(caminhoLogs)
-            x = calcula_Faturamento(y)
-            os.system('cls')
-            print(f"O faturamento total e de R${x:.2f}!")
-            time.sleep(1)
-            
-        elif opcao == 4:
-            y = leituraArquivo(caminhoLogs)
-            x = calcula_Percentual(y)
-            os.system('cls')
-            print(f"O valor percentual do produto escolhido e de {x:.2f}% !")
-            time.sleep(1)
-            
+        try:
+            opcao = int(input("Digite uma opção: "))
+            if opcao == 1:
+                produtos = get_dados_produtos()
+                salva_Arquivo(produtos, caminhoLogs)
+                os.system('cls')
+            elif opcao == 2:
+                os.system('cls')
+                y = leituraArquivo(caminhoLogs)
+                x = mostra_produtos(y)
+                print(x)
+            elif opcao == 3:
+                animacao()
+                y = leituraArquivo(caminhoLogs)
+                x = calcula_Faturamento(y)
+                os.system('cls')
+                print(f"O faturamento total é de R${x:.2f}!")
+                time.sleep(1)
+            elif opcao == 4:
+                y = leituraArquivo(caminhoLogs)
+                x = calcula_Percentual(y)
+                os.system('cls')
+                print(f"O valor percentual do produto escolhido é de {x:.2f}% !")
+                time.sleep(1)
+            elif opcao == 5:#Instalar a matPlotLib
+                produtos_do_arquivo = leituraArquivo(caminhoLogs)
+                gerar_grafico_matplotlib(produtos_do_arquivo, n=5)
+            else:
+                print("Opção inválida. Por favor, escolha uma opção válida.")
+        except ValueError:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+        time.sleep(2)
+        os.system('cls')
+
+##################################
+##Execucao principal do programa##
+##################################
 
 menu()
